f634d6ee6cc5695544d259b5dd5a5aa4d40b9ee2
Renvic <r.emenido03@gmail.com>
2024-06-05 01:04:36 +0800

    add

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-console.log('Boss Rv hahah')
-console.log("it's a prank")
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'login.html'));
+});
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'signup.html'));
+});
+
+app.listen(8000, () => 
+    console.log('Server listening on port 8000'));
